54369866d1fcb08a788038ba428a3c6fdf347867
CypherTux <62122038+cyphertux@users.noreply.github.com>
2025-02-11 17:21:35 +0100

    Update index.tsx

--- raffle-interface/pages/index.tsx
+++ raffle-interface/pages/index.tsx
@@ -27,7 +27,7 @@ export default function Home() {
   if (loading) {
     return (
       <div className="min-h-screen bg-gray-100 dark:bg-gray-900 flex items-center justify-center">
-        <GitHubForkButton /> {/* Ajouter ici */}
+        <GitHubForkButton />
         <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-blue-500 dark:border-blue-400"></div>
       </div>
     );
